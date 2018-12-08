362d8dbeec3fe992bb37af2f62f459bd079a5bc2
chiuc <chiuc@CHIUCS123>
2018-12-09 01:50:19 +0800

    Edit ContactsTest

--- src/cs3343_testcase/core/ContactsTest.java
+++ src/cs3343_testcase/core/ContactsTest.java
@@ -26,113 +26,112 @@ public class ContactsTest extends TestCase {
 		Node.reset();
 		Contacts.reset();
 	}
-	
+
 	@Test
 	public void testContacts() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 1 : testContacts()");
 		String name = "apple";
 		int age = 18;
-
 		Contacts c = new Contacts(name, age);
-
 		Contacts contacts = Contacts.getContactByName(name);
-
 		assertEquals(contacts.getName(), c.getName());
 	}
 
 	@Test
 	public void testAddStringInt() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 2 : testAddStringInt()");
 		String name = "apple";
 		int age = 18;
-
 		Contacts.add(name, age);
-
 		Contacts contacts = Contacts.getContactByName(name);
-
 		assertEquals(contacts.getName(), name);
 	}
 
 	@Test
 	public void testAddContacts() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 3 : testAddContacts()");
 		String name = "apple";
 		int age = 18;
-
 		Contacts c = Contacts.add(name, age);
-		
 		Contacts.remove(c);
 		Contacts.add(c);
-
 		Contacts contacts = Contacts.getContactByName(name);
-
 		assertEquals(contacts.getName(), name);
 	}
 
 	@Test
 	public void testRemoveString() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 4 : testRemoveString()");
 		String name = "apple";
 		int age = 18;
-
 		Contacts c = Contacts.add(name, age);
-		
 		Contacts.remove(c.getName());
-
 		Contacts contacts = Contacts.getContactByName(name);
-
 		assertEquals(contacts, null);
 	}
 
 	@Test
 	public void testRemoveContacts() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 5 : testRemoveContacts()");
 		String name = "apple";
 		int age = 18;
-
 		Contacts c = Contacts.add(name, age);
-		
 		Contacts.remove(c);
-
 		Contacts contacts = Contacts.getContactByName(name);
-
 		assertEquals(contacts, null);
 	}
 
 	@Test
 	public void testBindProperty() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 6 : testBindProperty()");
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testChooseApartment() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 7 : testChooseApartment()");
 		double positionX = 1, positionY = 0;
 		char index = 'e';
-
 		Estate e = (Estate) Map.addNode("estate", index, positionX, positionY);
-
 		String name = "apple";
 		int age = 18;
-
 		Contacts c = Contacts.add(name, age);
-
 		Apartments a = c.chooseApartment();
-
 		assertEquals(a.getEstate().getName(), e.getName());
 	}
 
 	@Test
 	public void testCheckNearestStation() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 8 : testCheckNearestStation()");
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCheckNearestStationInt() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 9 : testCheckNearestStationInt()");
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCheckNearestStationDistance() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 10 : testCheckNearestStationDistance()");
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCheckNearestStationDistanceInt() {
+		System.out.println("\n-----------------------------------------------------------");
+		System.out.println("Testcase 11 : testCheckNearestStationDistanceInt()");
 		fail("Not yet implemented");
 	}
 
